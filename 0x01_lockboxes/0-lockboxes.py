503d84034c939cda2f7187c84be5d5a7579da28c
Reganmatics <devreganmatics@gmail.com>
2023-01-06 06:17:35 +0100

    update README.md with test and expected output

---
+++ 0x01_lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+task that determines if all boxes can be opened, given n boxes,
+each box is numbered `0` to `n-1` and each may contain keys to the other boxes
+"""
+
+
+def canUnlockAll(boxes: list) -> bool:
+    """
+    args:
+        boxes -> list of lists
+    """
+    state: bool = False
+    return state
