92f65be8aeaad3a7d531484cac9ee43a65a94ea9
Reganmatics <devreganmatics@gmail.com>
2023-01-10 04:34:13 +0100

    add lockbx task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,10 +5,33 @@ each box is numbered `0` to `n-1` and each may contain keys to the other boxes
 """
 
 
-def canUnlockAll(boxes: list) -> bool:
+def canUnlockAll(boxes):
     """
     args:
-        boxes -> list of lists
+        boxes -> list
+
+    return -> bool (True/False)
     """
-    state: bool = False
-    return state
+    # Set to store the keys we have seen
+    keys = set()
+    # Queue for storing keys that we need to process
+    queue = []
+
+    # Add the keys in the first box to the queue
+    queue.extend(boxes[0])
+    # Mark the first box as visited
+    keys.add(0)
+
+    # Process keys in the queue
+    while queue:
+        # Get a key from the queue
+        key = queue.pop(0)
+        # If the key corresponds to a box, add the box's keys to the queue
+        # and mark the box as visited
+        if key < len(boxes) and key not in keys:
+            keys.add(key)
+            queue.extend(boxes[key])
+
+    # Return whether all boxes are visited or not
+    return len(keys) == len(boxes)
+
