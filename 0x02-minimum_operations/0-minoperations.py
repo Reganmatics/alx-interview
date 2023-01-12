c7fc03963fbb70eb9ab6b8af2a184d8c87a29675
Reganmatics <devreganmatics@gmail.com>
2023-01-12 23:45:41 +0100

    add minOp task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+0. Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+    args:
+        n -> int
+
+    return -> int
+    """
+    if n <= 0:
+        return 0
+    operations = 0
+    i = 2
+    while i <= n:
+        operations += 1
+        if n % i == 0:
+            n = n / i
+        else:
+            i += 1
+    return operations
+
