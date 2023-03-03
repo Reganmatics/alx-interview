ca0041ab324360a1e77e7b5b5c651d47c0bab453
Reganmatics <devreganmatics@gmail.com>
2023-03-03 04:58:55 +0100

    the rigt implementation for island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,15 +5,29 @@ island perimeter calculator
 
 
 def island_perimeter(grid):
-    """
-    compute the perimeter of island in grid
-    """
+    # Find the first land cell
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                # Use DFS to find the island and its perimeter
+                return dfs(i, j, grid)
 
-    height = len(grid)
-    width = len(grid[0])
-    count = 0
-    for row in grid:
-        count += row.count(1)
-    perimeter = 2 * (count + 1)
+def dfs(i, j, grid):
+    # Base case: cell is out of bounds or is water
+    if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+        return 1
+    # Base case: cell is already visited
+    if grid[i][j] == -1:
+        return 0
 
+    # Mark cell as visited
+    grid[i][j] = -1
+
+    # Recursively visit neighboring cells
+    perimeter = dfs(i+1, j, grid)
+    perimeter += dfs(i-1, j, grid)
+    perimeter += dfs(i, j+1, grid)
+    perimeter += dfs(i, j-1, grid)
+
+    # Return the perimeter contribution of this cell
     return perimeter
